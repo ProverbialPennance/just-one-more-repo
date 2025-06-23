0828863170d0314a4615ec6a3834dfcdf7232c9b
ProverbialPennance <36955346+ProverbialPennance@users.noreply.github.com>
2025-06-23 21:24:29 +0200

    modules: spaghetti-kart-nix - fix missed file rename

--- modules/default.nix
+++ modules/default.nix
@@ -3,7 +3,7 @@
     ./remote-play-whatever.nix
     ./sm64coopdx.nix
     ./xivlauncher-rb.nix
-    ./spaghetti-kart.nix
+    ./spaghetti-kart-git.nix
     ./starship-sf64.nix
     ./_2ship2harkinian-git.nix
     ./perfect-dark-git.nix
