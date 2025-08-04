f35a6b3788b44d8d5a607ce33cf60532b0daebad
ProverbialPennance <36955346+ProverbialPennance@users.noreply.github.com>
2025-08-04 21:09:17 +0200

    cockatrice: fe7853a3896b9bbe021085264f0f0fdfa9fd07f7 -> 2025-08-02-Development-2.11.0-beta.22-unstable-2025-08-02

--- pkgs/cockatrice/default.nix
+++ pkgs/cockatrice/default.nix
@@ -5,36 +5,38 @@
   cmake,
   protobuf,
   kdePackages,
-}:
-stdenv.mkDerivation (finalAttrs: {
-  pname = "cockatrice";
-  version = "fe7853a3896b9bbe021085264f0f0fdfa9fd07f7";
+}: let
+  rev' = "fe7853a3896b9bbe021085264f0f0fdfa9fd07f7";
+in
+  stdenv.mkDerivation (finalAttrs: {
+    pname = "cockatrice";
+    version = "2025-08-02-Development-2.11.0-beta.22-unstable-2025-08-02";
 
-  src = fetchFromGitHub {
-    owner = "Cockatrice";
-    repo = "Cockatrice";
-    rev = finalAttrs.version;
-    sha256 = "sha256-kKHoNtqnmoByS3e5Qeq76e/ARUlv7w4BgOGp9AWCt7E=";
-  };
+    src = fetchFromGitHub {
+      owner = "Cockatrice";
+      repo = "Cockatrice";
+      rev = "${rev'}";
+      sha256 = "sha256-zXAK830SdGT3xN3ST8h9LLy/oWr4MH6TZf57gLfI0e8=";
+    };
 
-  buildInputs = [
-    kdePackages.qtbase
-    kdePackages.qtmultimedia
-    protobuf
-    kdePackages.qttools
-    kdePackages.qtwebsockets
-  ];
+    buildInputs = [
+      kdePackages.qtbase
+      kdePackages.qtmultimedia
+      protobuf
+      kdePackages.qttools
+      kdePackages.qtwebsockets
+    ];
 
-  nativeBuildInputs = [
-    cmake
-    kdePackages.wrapQtAppsHook
-  ];
+    nativeBuildInputs = [
+      cmake
+      kdePackages.wrapQtAppsHook
+    ];
 
-  meta = {
-    homepage = "https://github.com/Cockatrice/Cockatrice";
-    description = "Cross-platform virtual tabletop for multiplayer card games";
-    license = lib.licenses.gpl2Plus;
-    maintainers = with lib.maintainers; [evanjs];
-    platforms = with lib.platforms; linux;
-  };
-})
+    meta = {
+      homepage = "https://github.com/Cockatrice/Cockatrice";
+      description = "Cross-platform virtual tabletop for multiplayer card games";
+      license = lib.licenses.gpl2Plus;
+      maintainers = with lib.maintainers; [evanjs];
+      platforms = with lib.platforms; linux;
+    };
+  })
