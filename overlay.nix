ee2a4ae7e32e0296c5c9cead30619a9694c633d2
Raine <64136899+raineaeternal@users.noreply.github.com>
2025-08-22 02:05:15 +0200

    [Factorio] Add headless experimental for base game (#164)
    * add 'custom' factorio experimental headless unit
    
    * forgot to fix this during the merge
    
    * always download latest experimental
    
    * update hash for factorio-headless_2.0.64

--- overlay.nix
+++ overlay.nix
@@ -8,10 +8,12 @@ final: prev: {
   shipwright-git = final.callPackage ./pkgs/shipwright-git {};
   perfect-dark-git = final.callPackage ./pkgs/perfect-dark-git {};
   dolphin-memory-engine = final.callPackage ./pkgs/dolphin-memory-engine {};
-  factorio-2_0_55 = final.callPackage ./pkgs/factorio/2.0.55 {releaseType = "headless";};
-  factorio-2_0_60 = final.callPackage ./pkgs/factorio/2.0.60 {releaseType = "headless";};
+  factorio-2_0_55 = final.callPackage ./pkgs/factorio/stable/2.0.55 {releaseType = "headless";};
+  factorio-2_0_60 = final.callPackage ./pkgs/factorio/stable/2.0.60 {releaseType = "headless";};
+  factorio-experimental = final.callPackage ./pkgs/factorio/experimental/latest {releaseType = "headless";};
   delta-patcher = final.callPackage ./pkgs/delta-patcher {};
   cockatrice-git = final.callPackage ./pkgs/cockatrice-git {};
+
   # sm64baserom = final.callPackage ./pkgs/sm64baserom {};
   # sm64ex-ap = final.callPackage ./pkgs/sm64ex-ap {sm64baserom = final.callPackage ./pkgs/sm64baserom {};};
 }
