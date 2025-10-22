ee073a006d5f10b64e15f08065202808a60201a6
Raine Aeternal <64136899+raineaeternal@users.noreply.github.com>
2025-10-22 04:15:00 +0200

    actually add stable as a path in overlay.nix

--- overlay.nix
+++ overlay.nix
@@ -11,6 +11,7 @@ final: prev: {
   factorio-2_0_55 = final.callPackage ./pkgs/factorio/stable/2.0.55 {releaseType = "headless";};
   factorio-2_0_60 = final.callPackage ./pkgs/factorio/stable/2.0.60 {releaseType = "headless";};
   factorio-2_0_66 = final.callPackage ./pkgs/factorio/stable/2.0.66 {releaseType = "headless";};
+  factorio-2_0_69 = final.callPackage ./pkgs/factorio/stable/2.0.69 {releaseType = "headless";};
   factorio-experimental = final.callPackage ./pkgs/factorio/experimental/latest {releaseType = "headless";};
   delta-patcher = final.callPackage ./pkgs/delta-patcher {};
   cockatrice-git = final.callPackage ./pkgs/cockatrice-git {};
