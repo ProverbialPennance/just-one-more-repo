8736b3ccbc1e5e7a317352364e74d918ddd0e71b
ProverbialPennance <36955346+ProverbialPennance@users.noreply.github.com>
2025-11-12 06:25:49 +0100

    correctly identify factorio packages, fix testScript

--- checks/factorio.nix
+++ checks/factorio.nix
@@ -3,9 +3,18 @@
   lib,
   pkgs,
 }: let
-  factorioPkgs = lib.filter (
-    name: lib.hasPrefix "factorio" name
-  ) (lib.attrNames self.packages);
+  factorioPkgs =
+    lib.filter (
+      elem: elem != null
+    ) (
+      lib.flatten (
+        lib.lists.unique (
+          lib.forEach (
+            lib.attrNames pkgs
+          ) (name: lib.match "(factorio-experimental|factorio-[[:digit:]].+)" name)
+        )
+      )
+    );
 in
   pkgs.testers.nixosTest (finalAttrs: {
     name = "Factorio";
@@ -17,37 +26,24 @@ in
 
         services.factorio = {
           enable = true;
-          package = self.pkgs."${p}";
+          package = pkgs."${p}";
         };
       };
     }));
 
-    # nodes = lib.listToAttrs (lib.forEach factorioPkgs (pkg: {
-    #   name = pkg;
-    #   value = {config, ...}: {
-    #     imports = [self.nixosModules.default];
-
-    #     services.factorio = {
-    #       enable = true;
-    #       package = pkg;
-    #     };
-    #   };
-    # }));
-
     globalTimeout = 1800;
 
     testScript = {nodes, ...}: ''
+      print(machines)
+
       start_all()
 
       for machine in machines:
         print(machine)
         print("waiting for factorio.service")
-        machine.wait_for_unit("network-online.target")
         machine.wait_for_unit("factorio.service")
 
         print("checking status")
         machine.succeed("systemctl status factorio.service --no-pager -l")
-
-      join_all()
     '';
   })
