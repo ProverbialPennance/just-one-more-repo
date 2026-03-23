bf586a6874b12b281be5ccd42e12d53240ef87c9
ProverbialPennance <36955346+ProverbialPennance@users.noreply.github.com>
2026-03-23 19:22:07 +0100

    checks: factorio - test script runs once more under current flake inputs

--- checks/factorio.nix
+++ checks/factorio.nix
@@ -18,6 +18,13 @@
 in
   pkgs.testers.nixosTest (finalAttrs: {
     name = "Factorio";
+    # REASON: the python test runner faults us with an error
+    # in the code we have to use to re-access the list of machines/vms
+    # being utilised. Previously this was accessible in our test's scope
+    # but it appears to have been removed. We are faking our way back to
+    # having it, but the typechecker will fault us without providing
+    # pedantic type hints.
+    skipTypeCheck = true;
 
     nodes = lib.listToAttrs (lib.forEach factorioPkgs (p: {
       name = p;
@@ -34,8 +41,12 @@ in
     globalTimeout = 1800;
 
     testScript = {nodes, ...}: ''
-      print(machines)
-
+      # at some point 'machines' got removed from the harness
+      # so we have to fake our way back around to it
+      scope = locals()
+      machines_q = scope.get("machines_qemu")
+      machines_n = scope.get("machines_nspawn")
+      machines = [*machines_q, *machines_n]
       start_all()
 
       for machine in machines:
