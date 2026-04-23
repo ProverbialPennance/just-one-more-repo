265564d65c139072085d98bc507a0ecf61530136
ProverbialPennance <36955346+ProverbialPennance@users.noreply.github.com>
2026-04-24 01:33:49 +0200

    replace `passthru.updateScript` with `unstableGitUpdater`
    add `perfect-dark-git` attr to `default.nix`

--- default.nix
+++ default.nix
@@ -2,4 +2,5 @@
   # This is needed for  the nix-build invocation done by nix-update
   xivlauncher-rb = pkgs.callPackage ./pkgs/xivlauncher-rb {};
   r2modman = pkgs.callPackage ./pkgs/r2modman {};
+  perfect-dark-git = pkgs.callPackage ./pkgs/perfect-dark-git {};
 }
