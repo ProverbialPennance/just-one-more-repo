5d4c9ee1011c385ed1c9c369036559d7165ff125
ProverbialPennance <36955346+ProverbialPennance@users.noreply.github.com>
2026-04-27 02:27:07 +0200

    [checks] friendly attr names

--- checks/default.nix
+++ checks/default.nix
@@ -1,5 +1,5 @@
 {lib, ...}: let
   paths = lib.filesystem.listFilesRecursive ./.;
 in {
-  allChecks = lib.genAttrs (lib.filter (pathString: !lib.strings.hasSuffix "default.nix" pathString) (lib.forEach paths (path: toString path)));
+  allChecks = lib.genAttrs (lib.filter (pathString: !lib.strings.hasSuffix "default" pathString) (lib.forEach paths (path: lib.strings.removeSuffix ".nix" (lib.lists.last (builtins.split "/" (toString path))))));
 }
