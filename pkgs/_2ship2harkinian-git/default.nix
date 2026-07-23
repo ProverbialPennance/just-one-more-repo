d2f2801d39d8dddffd4f23b38e99086ac579416a
ProverbialPennance <36955346+ProverbialPennance@users.noreply.github.com>
2026-07-23 02:58:53 +0200

    monocypher

--- pkgs/_2ship2harkinian-git/default.nix
+++ pkgs/_2ship2harkinian-git/default.nix
@@ -99,6 +99,13 @@
     hash = "sha256-zhRFEmPYNFLqQCfvdAaG5VBNle9Qm8FepIIIrT9sh88=";
   };
 
+  monocypher_src = fetchFromGitHub {
+    owner = "LoupVaillant";
+    repo = "Monocypher";
+    rev = "0d85f98c9d9b0227e42cf795cb527dff372b40a4";
+    hash = "sha256-RrM8Ep/CM7U5Q4+4FAHfBknb6b0upohoiqy4f7eMye0=";
+  };
+
   rev' = "65fb1b6e3cc49780057208f4050a160d1a4bdfc7";
 in
   stdenv.mkDerivation (finalAttrs: {
@@ -174,6 +181,7 @@ in
       (lib.cmakeFeature "FETCHCONTENT_SOURCE_DIR_STORMLIB" "${stormlib'}")
       (lib.cmakeFeature "FETCHCONTENT_SOURCE_DIR_PRISM" "${prism}")
       (lib.cmakeFeature "FETCHCONTENT_SOURCE_DIR_THREADPOOL" "${thread_pool}")
+      (lib.cmakeFeature "FETCHCONTENT_SOURCE_DIR_MONOCYPHER" "${monocypher_src}")
     ];
 
     dontAddPrefix = true;
