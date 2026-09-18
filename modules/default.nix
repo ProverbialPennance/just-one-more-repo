ddf2f57f28f773447980fedccde9cc984bf944db
ProverbialPennance <36955346+ProverbialPennance@users.noreply.github.com>
2026-09-18 11:54:46 +0200

    remove modules from deprecated packages

--- modules/default.nix
+++ modules/default.nix
@@ -3,14 +3,9 @@
     ./remote-play-whatever.nix
     ./sm64coopdx.nix
     ./xivlauncher-rb.nix
-    ./spaghetti-kart-git.nix
-    ./starship-sf64.nix
-    ./_2ship2harkinian-git.nix
-    ./shipwright-git.nix
     ./perfect-dark-git.nix
     ./dolphin-memory-engine.nix
     ./binfmt.nix
-    ./ghostship.nix
   ];
 
   nixpkgs.overlays = [
