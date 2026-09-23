d566569c3ed3f8ea53ef34adba3125f19833d154
ProverbialPennance <36955346+ProverbialPennance@users.noreply.github.com>
2026-09-23 19:42:43 +0200

    Use yet to be merged changes from nixpkgs#556060

--- pkgs/r2modman/default.nix
+++ pkgs/r2modman/default.nix
@@ -5,125 +5,127 @@
   dart-sass,
   electron,
   fetchFromGitHub,
+  fetchPnpmDeps,
   makeDesktopItem,
   makeWrapper,
   nodejs,
-  yarn-berry,
-}:
-stdenv.mkDerivation (finalAttrs: {
-  pname = "r2modman";
-  version = "3.2.18";
+  pnpm_11,
+  pnpmConfigHook,
+}: let
+  pnpm = pnpm_11;
+in
+  stdenv.mkDerivation (finalAttrs: {
+    pname = "r2modman";
+    version = "3.2.19";
 
-  src = fetchFromGitHub {
-    owner = "ebkr";
-    repo = "r2modmanPlus";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-QGs3kF2GkHlISmRb0cIYOKts1b1RvBj5qkc2cUPawwE=";
-  };
+    src = fetchFromGitHub {
+      owner = "ebkr";
+      repo = "r2modmanPlus";
+      tag = "v${finalAttrs.version}";
+      hash = "sha256-ZPwGAoYy3Q69QVgxictibydZPugEMdz4hpx52d7ScCU=";
+    };
 
-  missingHashes = ./missing-hashes.json;
-  offlineCache = yarn-berry.fetchYarnBerryDeps {
-    inherit (finalAttrs) src patches missingHashes;
-    hash = "sha256-6CwayFhy0ZwdL1ZOZVtCJLlchCv5raX7WF1V4TvVpq4=";
-  };
+    pnpmDeps = fetchPnpmDeps {
+      inherit (finalAttrs) pname version src;
+      inherit pnpm;
+      fetcherVersion = 4;
+      hash = "sha256-S32i+QGY6MWw+N9XS4A2K2HRfN1W6Wmf2PfaCfXDwNc=";
+    };
 
-  patches = [
-    # Make it possible to launch Steam games from r2modman.
-    ./steam-launch-fix.patch
+    patches = [
+      # Make it possible to launch Steam games from r2modman.
+      ./steam-launch-fix.patch
 
-    # Remove after upstream updates to Yarn 4.14
-    # https://github.com/ebkr/r2modmanPlus/blob/develop/package.json#L118
-    ./yarn-4.14-support.patch
+      # Fix copying of wrapper files to game directory
+      ./wrapper-fix.patch
+    ];
 
-    # Fix copying of wrapper files to game directory
-    ./wrapper-fix.patch
-  ];
+    __darwinAllowLocalNetworking = true;
 
-  __darwinAllowLocalNetworking = true;
+    nativeBuildInputs = [
+      copyDesktopItems
+      dart-sass
+      makeWrapper
+      nodejs
+      pnpm
+      pnpmConfigHook
+    ];
 
-  nativeBuildInputs = [
-    copyDesktopItems
-    dart-sass
-    makeWrapper
-    nodejs
-    yarn-berry
-    yarn-berry.yarnBerryConfigHook
-  ];
-
-  env = {
     # Required, as the build process won't have network access. Uses the wrapped electron binary instead.
-    ELECTRON_SKIP_BINARY_DOWNLOAD = true;
-  };
+    env.ELECTRON_SKIP_BINARY_DOWNLOAD = true;
 
-  postPatch = ''
-    # Hide update banner
-    echo "<template></template>" > src/components/banner/ManagerUpdateBanner.vue
-  '';
+    postPatch = ''
+      # Hide update banner
+      echo "<template></template>" > src/components/banner/ManagerUpdateBanner.vue
+    '';
 
-  buildPhase = ''
-    runHook preBuild
+    buildPhase = ''
+      runHook preBuild
 
-    substituteInPlace node_modules/sass-embedded/dist/lib/src/compiler-path.js \
-      --replace-fail 'compilerCommand = (() => {' 'compilerCommand = (() => { return ["${lib.getExe dart-sass}"];'
+      substituteInPlace node_modules/.pnpm/sass-embedded@*/node_modules/sass-embedded/dist/lib/src/compiler-path.js \
+        --replace-fail 'compilerCommand = (() => {' 'compilerCommand = (() => { return ["${lib.getExe dart-sass}"];'
 
-    yarn quasar build --mode electron --skip-pkg
+      pnpm quasar build --mode electron --skip-pkg
 
-    runHook postBuild
-  '';
+      # Remove unecessary devDependencies only required at build time.
+      pnpm prune --prod
 
-  installPhase = ''
-    runHook preInstall
+      runHook postBuild
+    '';
 
-    mkdir -p $out/share/r2modman
-    cp -r dist/electron/UnPackaged/. node_modules $out/share/r2modman
+    installPhase = ''
+      runHook preInstall
 
-    (
-      cd public/icons
-      for img in *png; do
-        dimensions=''${img#favicon-}
-        dimensions=''${dimensions%.png}
-        mkdir -p $out/share/icons/hicolor/$dimensions/apps
-        cp $img $out/share/icons/hicolor/$dimensions/apps/r2modman.png
-      done
-    )
+      mkdir -p $out/share/r2modman
+      cp -r dist/electron/UnPackaged/. node_modules $out/share/r2modman
 
-    makeWrapper '${lib.getExe electron}' "$out/bin/r2modman" \
-      --inherit-argv0 \
-      --add-flags "$out/share/r2modman" \
-      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}"
+      (
+        cd public/icons
+        for img in *png; do
+          dimensions=''${img#favicon-}
+          dimensions=''${dimensions%.png}
+          mkdir -p $out/share/icons/hicolor/$dimensions/apps
+          cp $img $out/share/icons/hicolor/$dimensions/apps/r2modman.png
+        done
+      )
 
-    runHook postInstall
-  '';
+      makeWrapper '${lib.getExe electron}' "$out/bin/r2modman" \
+        --inherit-argv0 \
+        --add-flags "$out/share/r2modman" \
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}"
 
-  desktopItems = [
-    (makeDesktopItem {
-      name = "r2modman";
-      exec = "r2modman %U";
-      icon = "r2modman";
-      desktopName = "r2modman";
-      comment = finalAttrs.meta.description;
-      categories = ["Game"];
-      mimeTypes = ["x-scheme-handler/ror2mm"];
-      keywords = [
-        "launcher"
-        "mod manager"
-        "thunderstore"
+      runHook postInstall
+    '';
+
+    desktopItems = [
+      (makeDesktopItem {
+        name = "r2modman";
+        exec = "r2modman %U";
+        icon = "r2modman";
+        desktopName = "r2modman";
+        comment = finalAttrs.meta.description;
+        categories = ["Game"];
+        mimeTypes = ["x-scheme-handler/ror2mm"];
+        keywords = [
+          "launcher"
+          "mod manager"
+          "thunderstore"
+        ];
+      })
+    ];
+
+    passthru.updateScript = ./update.sh;
+
+    meta = {
+      changelog = "https://github.com/ebkr/r2modmanPlus/releases/tag/v${finalAttrs.version}";
+      description = "Unofficial Thunderstore mod manager";
+      homepage = "https://github.com/ebkr/r2modmanPlus";
+      license = lib.licenses.mit;
+      mainProgram = "r2modman";
+      maintainers = with lib.maintainers; [
+        huantian
+        hythera
       ];
-    })
-  ];
-
-  passthru.updateScript = ./update.sh;
-
-  meta = {
-    changelog = "https://github.com/ebkr/r2modmanPlus/releases/tag/v${finalAttrs.version}";
-    description = "Unofficial Thunderstore mod manager";
-    homepage = "https://github.com/ebkr/r2modmanPlus";
-    license = lib.licenses.mit;
-    mainProgram = "r2modman";
-    # maintainers = with lib.maintainers; [
-    #   huantian
-    #   hythera
-    # ];
-    inherit (electron.meta) platforms;
-  };
-})
+      inherit (electron.meta) platforms;
+    };
+  })
